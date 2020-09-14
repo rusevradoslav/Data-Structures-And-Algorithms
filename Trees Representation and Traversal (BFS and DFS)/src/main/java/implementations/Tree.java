9bdfe6e22c03536346dc6ffb40473f87bb7aead8
rusevradoslav <radorusev98@gmail.com>
2020-09-14 16:40:13 +0300

    create Tree

---
+++ Trees Representation and Traversal (BFS and DFS)/src/main/java/implementations/Tree.java
@@ -0,0 +1,50 @@
+package implementations;
+
+import interfaces.AbstractTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tree<E> implements AbstractTree<E> {
+    private  E key ;
+    private Tree<E> parent;
+    private List<Tree<E>> children;
+
+    public Tree(E key, Tree<E>... children) {
+        this.key = key;
+        this.children = new ArrayList<>();
+        for (Tree<E> child : children) {
+            this.children.add(child);
+            child.parent = this;
+        }
+    }
+
+    @Override
+    public List<E> orderBfs() {
+
+        return null;
+    }
+
+    @Override
+    public List<E> orderDfs() {
+        return null;
+    }
+
+    @Override
+    public void addChild(E parentKey, Tree<E> child) {
+
+    }
+	
+	@Override
+    public void removeNode(E nodeKey) {
+
+    }
+
+    @Override
+    public void swap(E firstKey, E secondKey) {
+
+    }
+}
+
+
+
